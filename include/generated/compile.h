1007ade96baa9b95d5b12d6a795f51aafa18a310
carlos <carlos@carlos-laptop.(none)>
2012-06-03 16:29:58 -0500

    Initialize repo

---
+++ include/generated/compile.h
@@ -0,0 +1,9 @@
+/* This file is auto generated, version batteryExtenderRC2byLibiSC */
+/*  PREEMPT */
+#define UTS_MACHINE "arm"
+#define UTS_VERSION "#batteryExtenderRC2byLibiSC PREEMPT Sat Jun 2 18:08:36 PET 2012"
+#define LINUX_COMPILE_TIME "18:08:36"
+#define LINUX_COMPILE_BY "LibiSC"
+#define LINUX_COMPILE_HOST "BatteryExtender1.0"
+#define LINUX_COMPILE_DOMAIN "(none)"
+#define LINUX_COMPILER "gcc version 4.4.3 (GCC) "
